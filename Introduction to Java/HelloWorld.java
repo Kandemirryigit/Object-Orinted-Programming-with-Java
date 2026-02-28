bc6adf0d396fbbece60647002cf28799f84be64f
yiğit kandemir <kandemirryigit@gmail.com>
2026-02-28 08:18:46 -0800

    Add files via upload

---
+++ Introduction to Java/HelloWorld.java
@@ -0,0 +1,63 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
+
+
+
+
+// public class HelloWorld
+
+// This line declares a class named HelloWorld
+// In Java, everything resides inside a class.
+// The public keyword makes this class accessible from anywhere in your program
+// Classes are the fundamental building blocks of Java programs
+
+
+
+
+
+// public static void main(String[] args)
+
+// This line defines the main method
+// This is the entry point of your program 
+// the code that will be executed when you run your program.
+
+    // public
+    // Similar to the class declaration, public makes the main method accessible from outside the class.
+
+
+    // static
+    // The static keyword means that the main method belongs to the HelloWorld class itself, rather than to a specific instance of the class.
+
+
+    // void
+    // The void keyword indicates that the main method doesn't return any value.
+
+
+    // main(String[] args)
+    // This is the name of the method (main) and its parameters
+    // The String[] args part represents an array of strings that can be used to pass arguments to the program from the command line
+
+
+
+
+
+
+
+
+
+// System.out.println("Hello, World!");
+
+// This line is the heart of the program.
+// It prints the text "Hello, World!" to the console.
+
+   // System.out
+   // This is a standard output stream in Java. It represents the console.
+
+   // println()
+   // This is a method of the System.out object that prints a line of text to the console. 
+   // The text to be printed is enclosed in double quotes
+
---
+++ Introduction to Java/MyProgram.java
@@ -0,0 +1,11 @@
+public class MyProgram{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+
+        int x=5;
+        int y=10;
+        int sum=x+y;
+
+        System.out.println("The sum of "+x+" and "+y+" is: "+sum);
+    }
+}
